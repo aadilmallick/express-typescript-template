249911e17a923167c8389657b16d6ef89334c00f
aadilmallick <aadiltj.mallick@gmail.com>
2023-06-05 17:18:21 -0400

    add ejs and middleware folder

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,17 @@
 import { connectDB } from "./connectDb";
 import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
   // connectDB();
